2e80566b909097c180bc994a8a4bdcf6447a7d90
jkengs <107338480+jkengs@users.noreply.github.com>
2023-04-12 21:41:18 +1000

    feat: add server files

---
+++ ServerDictionary/src/main/java/IOHandlerException.java
@@ -0,0 +1,8 @@
+public class IOHandlerException extends Exception{
+
+    public IOHandlerException(){}
+
+    public IOHandlerException(String message) {
+        super(message);
+    }
+}
---
+++ ServerDictionary/src/main/java/InvalidRequestException.java
@@ -0,0 +1,8 @@
+public class InvalidRequestException extends Exception {
+
+    public InvalidRequestException(){}
+
+    public InvalidRequestException(String message) {
+        super(message);
+    }
+}
